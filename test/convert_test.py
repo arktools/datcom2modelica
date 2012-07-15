7fe72a60972e7c83b5df1a513ad79f3d1a52132d
Lenna Peterson <arklenna@gmail.com>
2012-07-14 21:50:02 -0400

    Added file integrity test.

--- test/convert_test.py
+++ test/convert_test.py
@@ -13,7 +13,15 @@ class TestConvert(unittest.TestCase):
     infile = 'test/data/Citation.out'
 
     def test_convert(self):
-        c = Convert(self.infile, "test/data/modelica_test")
+        # XXX this test should be alphabetically first XXX
+        Convert(self.infile, "test/data/modelica_test")
+
+    def test_integrity(self):
+        # XXX this test will fail if it's alphabetically before convert XXX
+        expected = open("test/data/modelica_expected.mo", 'rb').readlines()
+        test = open("test/data/modelica_test.mo", 'rb').readlines()
+        for e_line, t_line in zip(expected, test):
+            self.assertEqual(e_line, t_line)
 
 
 if __name__ == "__main__":
