d38a549b76bed6a1fdbcec6923a2dd68273b14fd
James Goppert <jgoppert@l1.(none)>
2012-07-14 20:21:51 -0400

    Added test folder.

---
+++ test/test.py
@@ -0,0 +1,18 @@
+'''
+Created on Jul 14, 2012
+
+@author: jgoppert
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
